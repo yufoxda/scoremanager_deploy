0fe3d6e2ea4f9989eb41146138e5736d22945ca7
yufoxda <147848207+yufoxda@users.noreply.github.com>
2024-08-12 21:07:51 +0900

    Add files via upload

---
+++ app_init_.py
@@ -0,0 +1,16 @@
+from database.createSQL import Book, Song, Author, PublishURL
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create the app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../onpuscores.db'
+db = SQLAlchemy(app)
+
+per_page = 30
+
+import rootes
---
+++ rootes.py
@@ -0,0 +1,34 @@
+from app_init_ import app,db,per_page
+from database.createSQL import Book, Song, Author, PublishURL
+from flask import render_template, request, url_for
+from flask_sqlalchemy import pagination 
+import os
+
+@app.route("/")
+def home():
+  return render_template('home.html')
+
+@app.route("/searchbook/",methods = ["GET"])
+def searchbook():
+  page = request.args.get('page', 1, type=int)
+  query = request.args.get('query')
+  app.logger.info(query)
+  books = db.session.query(Book).filter(Book.book_name.contains(query)).order_by(Book.book_name).paginate(page=page, per_page=30, error_out=False)
+
+  return render_template('searched_book.html',books = books,que = query,page=page)
+
+@app.route("/searchsong/",methods = ["GET"])
+def searchsong():
+  page = request.args.get('page', 1, type=int)
+  query = request.args.get('query')
+  app.logger.info(query)
+  songs = db.session.query(Song).filter(Song.song_name.contains(query)).order_by(Song.song_name).paginate(page=page, per_page=30, error_out=False)
+  app.logger.info(songs)
+  return render_template('searched_song.html',songs=songs,que = query,page=page)
+
+@app.route("/book/<int:id>")
+def bookinfo(id):
+  bookid = id
+  songs = db.session.query(Song).filter(Song.book_id == bookid).all()
+  bookname = db.session.query(Book).get(bookid)
+  return render_template('book.html',songs = songs,book = bookname)
